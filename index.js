0d7ac95b3d884a8f0825ef433c1a6c5bd473eb19
Caio <caioeduardojm4@gmail.com>
2021-12-13 16:51:25 -0300

    alteracao script

--- index.js
+++ index.js
@@ -2,13 +2,50 @@ var altura = 0
 var largura = 0
 
 function TamanhoAPP(){
-    var altura = window.innerHeight
-    var largura = window.innerWidth
+    altura = window.innerHeight
+    largura = window.innerWidth
 }
 TamanhoAPP()
 
-var posicaoX = Math.floor(Math.random() * largura)
-var posicaoY = Math.floor(Math.random() * altura)
+function Posicao(){
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
 
-var mosquito = document.createElement('img')
-document.body.appendChild(mosquito)
+    posicaoX = posicaoX < 0  ? 0 : posicaoX
+    posicaoY = posicaoY < 0  ? 0 : posicaoY
+    console.log(posicaoX, posicaoY)
+
+    var mosquito = document.createElement('img')
+    mosquito.src = './imagens/mosca.png'
+    mosquito.className = Classe() +' '+ Lados()
+    mosquito.style.left = posicaoX +'px'
+    mosquito.style.top = posicaoY +'px'
+    mosquito.style.position = 'absolute'
+
+
+    document.body.appendChild(mosquito)
+}
+
+function Classe(){
+    var classe = Math.floor(Math.random() * 3)
+
+    switch (classe){
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
+}
+
+function Lados(){
+    var lado = Math.floor(Math.random() * 2)
+
+    switch (lado){
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
+}
